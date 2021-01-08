b37c25cb420777dd6f4d586fe8ed3d7bd726c360
Shikamaru003 <alainbarredo@gmail.com>
2021-01-08 16:02:17 +0000

    commit

--- src/main/java/formacion/bb2/security/SecurityConstants.java
+++ src/main/java/formacion/bb2/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package formacion.bb2.security;
 public class SecurityConstants {
 
     public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final long EXPIRATION_TIME = 9999999;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 }
